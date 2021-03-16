29e6ea439cfa63a400a0e7825507590416b7ab3c
Nur Anggara Budhi Prakasa <80004705+Angg19@users.noreply.github.com>
2021-03-16 22:22:54 +0700

    Add files via upload

---
+++ Pertemuan2/Teori/MatematikaDemo.java
@@ -0,0 +1,11 @@
+public class MatematikaDemo {
+
+    public static void main(String args[]) {
+        matematika hasil = new matematika();
+        hasil.pertambahan(10, 20);
+        hasil.pengurangan(10, 5);
+        hasil.perkalian(10, 20);
+        hasil.pembagian(20, 2);
+        
+    }
+}
